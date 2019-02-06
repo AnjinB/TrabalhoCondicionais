2baaf2fb1fcc79ce09df33efaa94e63cdfdf9e59
Keisse Batista <38115547+AnjinB@users.noreply.github.com>
2019-02-06 16:49:40 -0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Veri_Salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sal1, sal2, sal3, med;
+
+            Console.WriteLine("digite o valor do salários: ");
+            sal1 = double.Parse(Console.ReadLine());
+            sal2 = double.Parse(Console.ReadLine());
+            sal3 = double.Parse(Console.ReadLine());
+
+            if (sal1 > sal2 && sal1 > sal3)
+            {
+                Console.WriteLine("O 1 salário é o maior!");
+            }
+            else if (sal2 > sal1 && sal2 > sal3)
+            {
+                Console.WriteLine("O 2 salário é o maior!");
+            }
+            else if (sal3 > sal1 && sal3 > sal2)
+            {
+                Console.WriteLine("O 3 salário é o maior!");
+            }
+            else
+            {
+                Console.WriteLine("salários iguais");
+            }
+            
+
+            med = (sal1 + sal2 + sal3) / 3;
+            Console.WriteLine("A média dos salários é: "+med.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadKey(true);
+        }
+    }
+}
